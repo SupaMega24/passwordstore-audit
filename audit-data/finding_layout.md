7be1844a87ba0fba52befb0a1a9d36ec5c7178e4
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-08-26 21:22:44 +0900

    Set up for reporting

---
+++ audit-data/finding_layout.md
@@ -0,0 +1,14 @@
+
+1. convince protocol of an issue
+2. how bad the issue is
+3. how to fix the issue
+
+### [S-#] TITLE (Root Cause + Impact)
+
+**Description:** 
+
+**Impact:** 
+
+**Proof of Concept:**
+
+**Recommended Mitigation:** 
--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.18; // ? Is this the correct version?
 
 /*
  * @author not-so-secure-dev
  * @title PasswordStore
- * @notice This contract allows you to store a private password that others won't be able to see. 
+ * @notice This contract allows you to store a private password that others won't be able to see.
  * You can update your password at any time.
  */
 contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    // @audit private variable not actually private.
+    // not safe to have password here.
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +25,9 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+
+    // @audit any user can set password!
+    // missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -30,6 +35,7 @@ contract PasswordStore {
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     // @audit no param in function
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
